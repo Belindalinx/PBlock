01646d0f4b74ab41af51f602292a7b5d74378437
belindalin <zercanopic@gmail.com>
2018-07-29 17:57:48 -0700

    Blockchain Practice

---
+++ main.py
@@ -0,0 +1,25 @@
+#initializing blockchain as a list
+blockchain=[]
+
+#get last blockchain value
+def pre_gl():
+    return blockchain[-1]
+
+#enter a new value
+def tx_amt():
+    tx_amt=float(input("your transaction amount?"))
+    return tx_amt
+
+#function for add value
+def add_value(tx, last_tx=0):
+    blockchain.append([last_tx, tx])
+    return blockchain
+
+#main
+def main():
+    add_value(tx_amt())
+    add_value(tx_amt(), pre_gl())
+    print(blockchain)
+    return
+
+main()
