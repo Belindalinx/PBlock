49da7a186019bb9cc858dae4c868d9e6c5f0e6d2
belindalin <zercanopic@gmail.com>
2018-07-31 11:55:43 -0700

    Add menu

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 blockchain=[]
 
 #get last blockchain value
-def pre_gl():
+def pre_led():
+    if len(blockchain)<1:
+        return None
     return blockchain[-1]
 
 #enter a new value
@@ -11,19 +13,47 @@ def tx_amt():
     return tx_amt
 
 #function for add value
-def add_value(tx, last_tx=0):
+def add_tx(tx, last_tx):
+    if last_tx==None:
+        last_tx=[0]
     blockchain.append([last_tx, tx])
     return blockchain
 
+#display blockchain
+def display_block():
+    for block in blockchain:
+        print(block)
+    return
+
+def menu():
+    print("""
+Menu:
+1. Add a new transaction"
+2. Display the block"
+Q. Quit
+    """)
+    choice=input("choose a function you want: ")
+    return choice
+
 #main
 def main():
-    add_value(tx_amt())
+    choice=""
+    while choice not in ["q","Q"]:
+        choice=menu()
 
-    while True:
-        add_value(tx_amt(), pre_gl())
+        if choice.upper()=="Q":
+            print("Good Bye")
 
-        for block in blockchain:
-            print(block)
+        elif choice=="1":
+            new_tx=tx_amt()
+            add_tx(new_tx, pre_led())
+
+        elif choice=="2":
+            display_block()
+
+        else:
+            print(choice, "is a invalid choice")
+
     return
 
 main()
